8e86580301ba7210d0e579ff2e6b294770f9975c
Lachlan Houston <LachlanHouston111@gmail.com>
2023-11-13 15:52:34 +0100

    update

--- DataLoader/DataLoader.py
+++ DataLoader/DataLoader.py
@@ -7,8 +7,9 @@ from torch.utils.data import DataLoader
 # Dataset class
 class QM9Dataset(Dataset):
     # Initialize the dataset object
-    def __init__(self, data):
+    def __init__(self, data, y_index=0):
         self.dataset = data
+        self.y_index = y_index
 
     # Return the length of the dataset
     def __len__(self):
@@ -22,8 +23,19 @@ class QM9Dataset(Dataset):
         atomic_numbers = batch.z
         coords = batch.pos
 
+        # q = atomic_numbers copied 3 times 
+        q = [num for num in atomic_numbers for i in range(3)]
+
+        # R_ij = 3x(len(atomic_numbers) * 3))
+        r_ij = torch.zeros((3, len(atomic_numbers) * 3))
+
+        # Get the distance between atoms
+        for i in range(r_ij.shape[0]):
+            for j in range(r_ij.shape[1]):
+                r_ij[i][j] = coords[j//3][i] - coords[j%3][i]
+
         # Variable to predict
-        y = batch.y
+        y = batch.y[0][self.y_index]
 
         # Combine the atomic numbers, coordinates and variable to predict
         return atomic_numbers, coords, y
@@ -32,12 +44,12 @@ def my_collate_fn(data):
         return data
 
 # Function to create the dataset
-def DataLoad(batch_size=1, shuffle=False, split=[0.8, 0.1, 0.1]):
+def DataLoad(batch_size=1, shuffle=False, split=[0.8, 0.1, 0.1], y_index=0):
     # Create the dataset
     data = QM9(root='./QM9')
 
     # Create the dataset object
-    dataset = QM9Dataset(data)
+    dataset = QM9Dataset(data, y_index=0)
 
     # Train-test split
     train_size = int(split[0] * len(dataset))
